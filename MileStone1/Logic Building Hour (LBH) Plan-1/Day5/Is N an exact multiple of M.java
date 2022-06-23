6b6e299c9c6f850375b39820368169170a97ab81
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2022-06-23 22:40:23 +0530

    Create Is N an exact multiple of M.java

---
+++ MileStone1/Logic Building Hour (LBH) Plan-1/Day5/Is N an exact multiple of M.java
@@ -0,0 +1,9 @@
+/***
+Is N an exact multiple of M
+***/
+public static int nMultipleM(int input1,int input2){
+        if( input1 == 0 || input2 == 0) return 3;
+        else if(input1%input2 == 0) return 2;
+        else return 1;
+
+    }
